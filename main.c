34f952a08fa72eed8af38be5c1881a9905aed837
beatriz <biazinha_vascao@hotmail.com>
2015-05-25 16:24:20 -0300

    Menu e escolha de opções em andamento...

--- main.c
+++ main.c
@@ -20,36 +20,103 @@
  * 
  */
 
-int leArquivo(Legenda *L){
+int menu() {
+    int escolha;
+    printf("---------------------------MENU-------------------------------\n");
+    printf("| 1 - Inserir legenda                                        |\n");
+    printf("| 2 - Remover uma Legenda                                    |\n");
+    printf("| 3 - Consultar legendas                                     |\n");
+    printf("| 4 - Atualizar Legendas                                     |\n");
+    printf("| 5- Atualizaar Tempo                                        |\n");
+    printf("| 6 - Sair                                                   |\n");
+    printf("\n");
+    printf("Qual a atividade desejada? ");
+    scanf("%d", &escolha);
+
+    return escolha;
+}
+
+int leArquivo(Legenda *L, char nomeDoArquivo[30]) {
     int numero;
     char inicial[13], final[13], seta[4];
     Texto t = NULL;
-    
-    FILE *arq  = fopen(nomeDoArquivo, 'r');
-    if(!(*arq)){
+
+    FILE *arq = fopen(nomeDoArquivo, 'r');
+    if (!(*arq)) {
         return 0;
     }
-    while(fscanf(arq, "%d",&numero) != EOF){
-        sscanf(arq,"%s%s%s",inicial,seta,final);
+    while (fscanf(arq, "%d", &numero) != EOF) {
+        sscanf(arq, "%s%s%s", inicial, seta, final);
         t = leTextoLegenda(arq);
-        inserirLegenda(*L, numero,inicial, final, t);
+        * L = inserirLegenda(*L, numero, inicial, final, t);
     }
-
+    fclose(arq);
+    return 1;
 }
 
 int main(int argc, char** argv) {
+    FILE * arq = NULL;
     Legenda L = NULL;
     char nomeDoArquivo[30];
     printf("Digite o nome do arquivo: \n");
     scanf(" %30[^\n]", nomeDoArquivo);
 
     while (strstr(nomeDoArquivo, ".srt") == NULL) {
-       printf("Digite o nome do arquivo com a extensão '.srt': ");
-       scanf(" %30[^\n]", nomeDoArquivo);
+        printf("Digite o nome do arquivo com a extensão '.srt': ");
+        scanf(" %30[^\n]", nomeDoArquivo);
+    }
+    if (!leArquivo(&L, nomeDoArquivo)) {
+        arq = fopen(nomeDoArquivo, "w");
+    }
+    int menu;
+    while ((menu = menu()) != 6) {
+
+        switch (menu) {
+            case 1:
+                int numLegenda;
+                char tempoInicial[13], tempoFinal[13];
+                Texto t;
+                printf("Digite o número da legenda");
+                scanf("%d", &numLegenda);
+                printf("Digite o TEMPO INICIAL: ");
+                scanf("%s", &tempoInicial);
+                printf("Digite o TEMPO FINAL: ");
+                scanf("%s", &tempoFinal);
+                printf("Escreva o texto da legenda: ");
+                t = escreveLegenda();
+                L = inserirLegenda(L,numLegenda,tempoInicial,tempoFinal,t);                
+                break;
+            case 2:
+                int numLegenda;
+                printf("Digite o número da legenda a ser retirada: ");
+                scanf("%d", &numLegenda);
+                retiraLegenda(L,numLegenda);
+                printf("Legenda Retirada com Sucesso!");
+                break;
+            case 3:
+                // ???????
+                break;
+            case 4:
+                int numLegenda;
+                char tempoInicial[13], tempoFinal[13];
+                Texto t;
+                
+                printf("Digite o número da legenda");
+                scanf("%d", &numLegenda);
+                printf("Digite o TEMPO INICIAL: ");
+                scanf("%s", &tempoInicial);
+                printf("Digite o TEMPO FINAL: ");
+                scanf("%s", &tempoFinal);
+                printf("Escreva o texto da legenda: ");
+                t = escreveLegenda();
+                atualizaLegenda(L,tempoInicial,tempoFinal,t,numLegenda);
+                printf("Legenda atualizada com sucesso!");
+                break;
+            case 5:
+                break;
+        }
     }
 
-    leArquivo(&L);
-    
     return (EXIT_SUCCESS);
 }
 
@@ -63,6 +130,6 @@ int main(int argc, char** argv) {
     
     return (EXIT_SUCCESS);
 }
-*/
+ */
 
 
